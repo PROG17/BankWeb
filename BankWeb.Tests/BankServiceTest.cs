300ee5a6ab100876eda156a94fe34a0d755adb28
username <test@example.com>
2018-11-07 14:40:55 +0100

    lagt till unit test

---
+++ BankWeb.Tests/BankServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankWeb.Tests
+{
+    [TestClass]
+    public class BankServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
